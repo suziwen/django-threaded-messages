848c55570988eab5e5f9976bcf82bf4b0d4db1fa
Philipp Wassibauer <phil@gidsy.com>
2011-11-08 16:18:10 +0100

    listeners fix

--- threaded_messages/listeners.py
+++ threaded_messages/listeners.py
@@ -4,13 +4,15 @@ import logging
 logger = logging.getLogger('gidsy.apps.sendgrid')
 
 if sendgrid_settings.THREADED_MESSAGES_USE_SENDGRID:
-    from sendgrid_parse_api.utils import email_received
+    from sendgrid_parse_api.signals import email_received
+else:
+    email_received = None
 
 def signal_received_email(sender, sma, app_id, html, text, from_field, **kwargs):
     from utils import reply_to_thread, strip_quotes # circular dependency fix
     logger.debug("Sendgrid signal receive: %s, %s, %s, %s, %s, %s"%(sender, sma, app_id,
                                                                     html, text, from_field) )
-    if app_id == settings.THREADED_MESSAGES_ID:
+    if app_id == sendgrid_settings.THREADED_MESSAGES_ID:
         body =''
 
         if text:
@@ -25,5 +27,6 @@ def signal_received_email(sender, sma, app_id, html, text, from_field, **kwargs)
             reply_to_thread(thread, sma.user, body)
 
 def start_listening():
-    logger.debug("Sendgrid start listening")
-    email_received.connect(signal_received_email, dispatch_uid="thm_reply")
+    if email_received:
+        logger.debug("Sendgrid start listening")
+        email_received.connect(signal_received_email, dispatch_uid="thm_reply")
