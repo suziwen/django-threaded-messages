33eb4182c5ef07e078cc9b1abf6a656a5555050e
Jannis Leidel <jannis@leidel.info>
2012-01-19 16:57:34 +0100

    Fixed packaging.

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ setup(
     description='User-to-user threaded messaging system (similar to facebook) for Django',
     author='Philipp Wassibauer, John Debs',
     author_email='phil@gidsy.com',
-    url='https://github.com/philippWassibauer/django-threaded-messages',
-    download_url='https://github.com/philippWassibauer/django-threaded-messages',
+    url='https://github.com/Gidsy/django-threaded-messages',
+    download_url='https://github.com/Gidsy/django-threaded-messages',
     packages=(
         'threaded_messages',
+        'threaded_messages.migrations',
         'threaded_messages.templatetags',
     ),
     package_data={
         'threaded_messages': [
             'templates/django_messages/*.html',
-            'locale/*/LC_MESSAGES/*.po',
+            'locale/*/LC_MESSAGES/*',
             'static/js/*.js',
             'static/css/*.css',
-            'migrations/*.py'
-        ]
+        ],
     },
     classifiers=(
         'Development Status :: 4 - Beta',
