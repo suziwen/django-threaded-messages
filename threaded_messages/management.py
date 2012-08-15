cadc2e47bc3b147cd7761517a4edd5a621a87cf5
Philipp Wassibauer <phil@gidsy.com>
2012-08-15 10:58:10 +0200

    changed wording for notifications, bumped version

--- threaded_messages/management.py
+++ threaded_messages/management.py
@@ -5,7 +5,7 @@ from django.db.models import signals
 if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("received_email", _("Received a Message"), _("Someone has sent you a message"))
+        notification.create_notice_type("received_email", _("Private messages"), _("(this is highly recommended)"))
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 else:
     print "Skipping creation of NoticeTypes (Threaded Messages) as notification app not found"
