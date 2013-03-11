6a946104817eda2f24b72d74c905049216a12d59
suziwen <suziwen1@gmail.com>
2013-03-11 14:16:42 +0800

    modified for match notification version 1.0

--- threaded_messages/management.py
+++ threaded_messages/management.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.db.models import signals
 
 if "notification" in settings.INSTALLED_APPS:
-    from notification import models as notification
+    from notification.models import NoticeType
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("received_email", "Private messages", "(this is highly recommended)")
-    signals.post_syncdb.connect(create_notice_types, sender=notification)
+        NoticeType.create("received_email", "Private messages", "(this is highly recommended)")
+    import notification
+    signals.post_syncdb.connect(create_notice_types, sender=notification.models)
 else:
     print "Skipping creation of NoticeTypes (Threaded Messages) as notification app not found"
